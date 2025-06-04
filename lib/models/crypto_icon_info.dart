3c17ac1b2facc60c0b8ac8d972298509cb6217e7
Aurum <aurumos@outlook.com>
2025-06-04 22:06:16 +0330

    Add files via upload

---
+++ lib/models/crypto_icon_info.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart'; // For Color
+
+// Class for storing icon path and color for crypto icons
+class CryptoIconInfo {
+  final String iconPath;
+  final Color color;
+
+  const CryptoIconInfo({required this.iconPath, required this.color});
+}
