a499f13933fc25b3ef2cc3e1155a95f4f27ddfa3
aurumco <aurumco@users.noreply.github.com>
2025-06-05 17:10:47 +0000

    deploy: db7d4e57cea2011c0cf8e3eb99d0cffb1d61f894

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"83bacfc52569459a4a654727cad2546820cb0d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1558059761"
+    serviceWorkerVersion: "3123293346"
   }
 });
