b2335cedefcf8a4c30d6dc95055eea717e7d9e0d
Aurum <aurumos@outlook.com>
2025-06-26 21:58:19 +0330

    Delete lib directory

--- lib/models/crypto_icon_info.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart'; // For Color
-
-// Class for storing icon path and color for crypto icons
-class CryptoIconInfo {
-  final String iconPath;
-  final Color color;
-
-  const CryptoIconInfo({required this.iconPath, required this.color});
-}
