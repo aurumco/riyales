c0bc60b9ac3e8fd4188e8cac6f7549f96ca1b8f8
Aurum <aurumos@outlook.com>
2025-07-10 02:27:45 +0330

    Delete lib directory

--- lib/config/constants.dart
+++
@@ -1,7 +0,0 @@
-// lib/config/constants.dart
-
-class AppConstants {
-  static const String apiBaseUrl = 'https://auth.ryls.ir';
-  static const String deviceEndpoint = '/device';
-  static const String eventEndpoint = '/event';
-}
--- lib/localization/l10n_utils.dart
+++
@@ -1,3 +0,0 @@
-// Re-export generated localization classes
-export 'package:flutter_localizations/flutter_localizations.dart';
-export '../generated/app_localizations.dart'; // Re-export the generated AppLocalizations class
